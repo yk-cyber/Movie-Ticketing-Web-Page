68e4c971d174588ee34eef0f053cc1584d05b8e4
yk-cyber <151984166+yk-cyber@users.noreply.github.com>
2026-04-02 15:23:11 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/Movie-Ticketing-Web-Page/",
   plugins: [
     react(),
     tailwindcss(),
