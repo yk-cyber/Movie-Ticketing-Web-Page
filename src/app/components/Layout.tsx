41bb81f5ab555b51043449c7c45f0fa8468224dd
yk-cyber <chiayikuang714@gmail.com>
2026-04-03 16:21:42 +0800

    gg

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import { useEffect } from "react";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 
 export const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="min-h-screen flex flex-col bg-black text-zinc-100 font-sans selection:bg-red-900 selection:text-white">
       <Navbar />
